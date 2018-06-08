57453a626943f7e5c88b5ab86fb566111b4f2315
Equan Simmonds <esimmonds@ncarb.org>
2018-06-08 16:01:04 -0400

    Incident Query

---
+++ RightNowTestHarness/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+
+using System.Net;
+
+namespace RightNowTestHarness
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            Console.WriteLine("Starting RightNow Test Harness");
+
+            RightNowTestHarness rightNowTestHarness = new RightNowTestHarness();
+            rightNowTestHarness.TestIncidentQuery();
+
+        }
+    }
+}
---
+++ RightNowTestHarness/RightNowTestHarness.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using System.ServiceModel;
+using System.Web.Services.Protocols;
+using BasicSamples.RightNowService;
+
+namespace RightNowTestHarness
+{
+    class RightNowTestHarness
+    {
+        readonly RightNowSyncPortClient client;
+
+        public RightNowTestHarness()
+        {
+            this.client = new RightNowSyncPortClient();
+            this.client.ClientCredentials.UserName.UserName = "<username>";
+            this.client.ClientCredentials.UserName.Password = "<password>";
+        }
+
+        public void TestIncidentQuery()
+        {
+            try
+            {
+                QueryIncident("180326-000222");
+
+            }
+            catch (FaultException ex)
+            {
+                Console.WriteLine(ex.Code);
+                Console.WriteLine(ex.Message);
+            }
+            catch (SoapException ex)
+            {
+                Console.WriteLine(ex.Code);
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void QueryIncident(string incidentNumber)
+        {
+            Console.WriteLine("Querying Incident #" + incidentNumber);
+
+            const int MaxRows = 1;
+
+            var queryHeader = new ClientInfoHeader
+            {
+                AppID = "Query Incident by Reference Number"
+            };
+
+            var queryString = $"SELECT Incident FROM Incident WHERE Incident.ReferenceNumber='{incidentNumber}'";
+
+            var incident = new Incident
+            {
+                StatusWithType = new StatusWithType { Status = new NamedID() },
+                Threads = new Thread[] { }
+            };
+
+            var queryResultData = this.client.QueryObjects(queryHeader, queryString, new RNObject[] { incident }, MaxRows);
+
+            var rnObjectsResult = queryResultData.First().RNObjectsResult;
+
+            incident = rnObjectsResult.First() as Incident;
+
+            Console.WriteLine("Result Data Length " + queryResultData.Length);
+
+            if (incident != null)
+            {
+                Console.WriteLine("Incident Subject :" + incident.Subject);
+            }
+        }
+    }
+}
